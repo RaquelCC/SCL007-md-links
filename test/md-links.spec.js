6013c593c4c75f65ccab78238b989d89c0061d16
RaquelCC <rcanalesconcha@gmail.com>
2019-02-20 16:03:38 -0300

    tests tentativos

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,9 +1,41 @@
-const mdLinks = require('../');
+const mdLinks = require('../index.js');
 
 
 describe('mdLinks', () => {
 
-  it('should...', () => {
+  it('Debería ser una función', () => {
+    console.log('FIX ME!');
+  });
+
+  it('Debería leer archivos .md', () => {
+    console.log('FIX ME!');
+  });
+
+  it('Debería retornar la cantidad de links que contiene el archivo .md analizado', () => {
+    console.log('FIX ME!');
+  });
+
+  it('Debería retornar los links que contiene el archivo .md analizado', () => {
+    console.log('FIX ME!');
+  });
+
+  it('Al aplicar la opción "validate" debería indicar qué links estan rotos', () => {
+    console.log('FIX ME!');
+  });
+
+  it('Si el path provisto por el usuario es una carpeta, debería analizar todos los archivos .md en esa carpeta', () => {
+    console.log('FIX ME!');
+  });
+
+  it('Si el path provisto por el usuario es una carpeta, debería analizar las sub-carpetas de esta buscando archivos .md para analizar', () => {
+    console.log('FIX ME!');
+  });
+
+  it('Al aplicar la opción "line" debería indicar la linea en que se encuentra el archivo roto', () => {
+    console.log('FIX ME!');
+  });
+
+  it('Al analizar multiples archivos .md debería indicar qué links pertenecen a qué archivo', () => {
     console.log('FIX ME!');
   });
 
